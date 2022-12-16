2e372e8e1409be07f10b9690c80eeb7e9f5d5dff
Harshit Ratan <harshitratanshukla@gmail.com>
2022-12-16 18:33:42 +0530

    Card is design

---
+++ src/component/MyCard.jsx
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/system/Stack';
+import CardActions from '@mui/material/CardActions';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+
+export default function MyCard() {
+    return (
+        <Card
+            sx={{
+                maxWidth: 345,
+                border: '2px solid #b2b2b2',
+                borderRadius: '10px',
+                boxShadow: 'inset 0 -15px 0 0 #750b0b'
+            }}
+        >
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={0}
+                sx={{
+                    width: '100%',
+                    height: '100%',
+                    marginBottom: '25px'
+                }}
+            >
+                <CardMedia
+                    component="img"
+                    image="./images/photo_1.jpg"
+                    alt="tablet-image"
+                    sx={{
+                        borderRadius: '10px',
+                        height: '250px',
+                        width: '95%',
+                        marginTop: '8px'
+                    }}
+                />
+                <Typography
+                    gutterBottom
+                    variant="body1"
+                    sx={{
+                        fontWeight: '800',
+                        margin: '0px',
+                        marginTop: '10px'
+                    }}
+                >
+                    Favipiravir 400mg (Fabiflu) Tablets
+                </Typography>
+                <Typography
+                    gutterBottom
+                    variant="h6"
+                    sx={{
+                        fontWeight: '800',
+                        margin: '0px'
+                    }}
+                >
+                    <span style={{ color: '#740b09' }}> Rs 1,775 </span>/
+                    <span style={{ color: '#686868', fontSize: '14px' }}> Stripe</span>
+                </Typography>
+                <Typography
+                    variant="body1"
+                    sx={{
+                        fontWeight: '600',
+                        marginTop: '20px'
+                    }}
+                >
+                    Glenmark Pharmaceutical Limited
+                </Typography>
+                <Typography
+                    variant="body2"
+                    sx={{
+                        margin: '0px',
+                        fontWeight: '600',
+                        textTransform: 'uppercase',
+                        color: '#686868'
+                    }}
+                >
+                    Parvat Patiya, Surat, Gujrat
+                </Typography>
+            </Stack>
+            <Divider />
+            <CardActions sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <IconButton sx={{ borderRadius: '0px', color: '#168687', fontWeight: '600', mb: 1 }}>
+                    <TelegramIcon fontSize="large" sx={{ mr: 1 }} />
+                    Contact Supplier
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
+}
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,18 +3,22 @@ import Container from "@mui/system/Container";
 import Stack from '@mui/material/Stack';
 import MyBreadCrumbs from "../component/MyBreadCrumbs";
 import SearchFieldComponent from "../component/SearchFieldComponent";
+import MyCard from "../component/MyCard";
+import Grid from '@mui/material/Grid';
 
 const Home = () => {
   const [searchText, setSearchText] = React.useState("");
+  const [data, setData] = React.useState(["1", "2", "3", "4", "5", "6"]);
+
   const handleSearch = () => {
     console.log("searchText :::: ", searchText);
   }
 
   return (
     <Container
-      sx={{
-        border: '2px solid red'
-      }}
+    // sx={{
+    //   border: '2px solid red'
+    // }}
     >
       <MyBreadCrumbs />
       <Stack
@@ -22,10 +26,19 @@ const Home = () => {
         justifyContent="flex-start"
         alignItems="center"
         spacing={0}
-        sx={{ width: '100%' }}
+        sx={{ width: '100%', mb: 4 }}
       >
         <SearchFieldComponent value={searchText} setValue={setSearchText} handleSearch={handleSearch} />
       </Stack>
+      <Grid container spacing={2}>
+        {
+          data.map((value) => (
+            <Grid item xs={12} sm={6} md={4} key={value}>
+              <MyCard />
+            </Grid>
+          ))
+        }
+      </Grid>
     </Container>
   );
 };
